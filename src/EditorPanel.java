0f73af718320f3232ee3658aa58fa1b00cdabd45
NickSwings <raunaknickdeybwn001@gmail.com>
2026-04-15 18:52:01 +0530

    refactor: change panel repaint condition

--- src/EditorPanel.java
+++ src/EditorPanel.java
@@ -1,10 +1,13 @@
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import java.awt.*;
 import java.io.File;
 
 public class EditorPanel extends JPanel {
     private JScrollPane scrollPane;
     private JTextPane textPane;
+    private LineNumberPanel lineNumberPanel;
     private File file;
 
     public EditorPanel(String content) {
@@ -12,7 +15,14 @@ public class EditorPanel extends JPanel {
         textPane = new JTextPane();
         textPane.setText(content);
         scrollPane = new JScrollPane(textPane);
+        lineNumberPanel = new LineNumberPanel(textPane);
+        scrollPane.setRowHeaderView(lineNumberPanel);
         add(scrollPane, BorderLayout.CENTER);
+        textPane.getDocument().addDocumentListener(new DocumentListener() {
+            public void insertUpdate(DocumentEvent e) {scrollPane.repaint();}
+            public void removeUpdate(DocumentEvent e) {scrollPane.repaint();}
+            public void changedUpdate(DocumentEvent e) {}
+        });
         file=null;
     }
 
